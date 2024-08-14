9d941e834cf0869ef24611f210f884bc2518227f
Krunal Dodiya <kunal.dodiya1@gmail.com>
2024-08-14 12:46:19 +0530

    init

--- main.py
+++ main.py
@@ -24,17 +24,21 @@ app.add_middleware(SessionMiddleware, secret_key="secret_key")
 
 app.add_exception_handler(
     InertiaVersionConflictException,
-    inertia_version_conflict_exception_handler,
+    inertia_version_conflict_exception_handler,  # type: ignore[arg-type]
 )
 
 app.add_exception_handler(
     RequestValidationError,
-    inertia_request_validation_exception_handler,
+    inertia_request_validation_exception_handler,  # type: ignore[arg-type]
+)
+
+manifest_json = os.path.join(
+    os.path.dirname(__file__), "webapp", "dist", "client", "manifest.json"
 )
 
 vue_dir = (
-    os.path.join(os.path.dirname(__file__), "webapp", "dist")
-    if inertia_config.environment != "development"
+    os.path.join(os.path.dirname(__file__), "webapp", "dist", "client")
+    if inertia_config.environment != "development" or inertia_config.ssr_enabled is True
     else os.path.join(os.path.dirname(__file__), "webapp", "src")
 )
 
